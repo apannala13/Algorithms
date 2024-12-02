cf11a255612405e1df07b662a27cb53a3832f60f
apannala13 <145242587+apannala13@users.noreply.github.com>
2024-12-02 15:41:47 -0800

    Delete neetcode-all/arrays_and_hashing/is_prefix_of_word.py

--- neetcode-all/arrays_and_hashing/is_prefix_of_word.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
-        words = sentence.split()
-
-        for i, word in enumerate(words):
-            if word[:len(searchWord)] == searchWord:
-                return i + 1
-        return -1
